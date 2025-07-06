d3940238f22b35f2388b22debacbde34aef7b846
Zihad550 <dev.jehadhossain@gmail.com>
2025-07-06 21:04:59 +0600

    refresh token

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,40 +1,73 @@
 import {
-  createApi,
-  fetchBaseQuery,
   type BaseQueryApi,
   type BaseQueryFn,
   type DefinitionType,
   type FetchArgs,
+  createApi,
+  fetchBaseQuery,
 } from "@reduxjs/toolkit/query/react";
+import { toast } from "sonner";
+import { logout, setUser } from "../features/auth/authSlice";
 import type { RootState } from "../store";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: import.meta.env.VITE_APP_API_URL,
+  credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token;
-    if (token) headers.append("authorization", token);
+
+    if (token) {
+      headers.set("authorization", `${token}`);
+    }
+
     return headers;
   },
-  credentials: "include",
 });
 
-const baseQueryWithToken: BaseQueryFn<
+const baseQueryWithRefreshToken: BaseQueryFn<
   FetchArgs,
   BaseQueryApi,
   DefinitionType
 > = async (args, api, extraOptions): Promise<any> => {
-  const result = await baseQuery(args, api, extraOptions);
-  if (result?.error?.status === 500) {
-    // console.log("global error ->", result);
-    // TODO: unauthorized logout
-    return;
+  let result = await baseQuery(args, api, extraOptions);
+
+  if (result?.error?.status === 404) toast.error(result?.error?.data.message);
+
+  if (result?.error?.status === 403) toast.error(result?.error?.data.message);
+
+  if (result?.error?.status === 401) {
+    const res = await fetch(
+      `${import.meta.env.VITE_APP_API_URL}/auth/refresh-token`,
+      {
+        method: "POST",
+        credentials: "include",
+      },
+    );
+
+    const data = await res.json();
+
+    if (data?.data?.accessToken) {
+      const user = (api.getState() as RootState).auth.user;
+
+      api.dispatch(
+        setUser({
+          user,
+          token: data.data.accessToken,
+        }),
+      );
+
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      api.dispatch(logout());
+    }
   }
+
   return result;
 };
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: baseQueryWithToken,
+  baseQuery: baseQueryWithRefreshToken,
   tagTypes: [
     "products",
     "user-orders",
